30d29113e88c827bbd9fe72db0500d81d2f151a2
vivz <vivianzhang61@gmail.com>
2023-07-15 17:34:16 -0400

    properly working gradient

--- ion_spacing.jl
+++ ion_spacing.jl
@@ -37,7 +37,7 @@ function get_total_potential(trap::TrapVoltage, ion_positions::Vector{Float64})
     total_potential = 0.0
     for i in eachindex(ion_positions)
         trap_potential = get_voltage(trap, ion_positions[i])
-        coulomb_potential = get_coulomb_potential_at_x(ion_positions, ion_positions[i])
+        coulomb_potential = get_coulomb_potential_at_x(ion_positions, i)
         # divide by two because we counted twice
         total_potential = total_potential + trap_potential + coulomb_potential / 2.0
     end
@@ -48,7 +48,7 @@ function calculate_gradient!(G, trap::TrapVoltage, ion_positions::Vector{Float64
     # Calculate total potential's gradient wrt each ion position 
     for i in eachindex(G)
         x = ion_positions[i] / EURIQA_x0
-        trap_gradient = EURIQA_v0 * (trap.x1 + trap.x2 * x + trap.x3 * x^2 / 2.0 + trap.x4 * x^3 / 6.0)
+        trap_gradient = EURIQA_v0 / EURIQA_x0 * (trap.x1 + trap.x2 * x + trap.x3 * x^2 / 2.0 + trap.x4 * x^3 / 6.0) 
         coulomb_gradient = 0.0
         for j in eachindex(ion_positions)
             if j != i
@@ -66,12 +66,12 @@ function get_ion_spacing_for_voltage(trap::TrapVoltage)
     initial_ion_spacing = collect(LinRange(-num_ion / 2, num_ion / 2, num_ion)) * 1e-6
     res = optimize(
             params -> get_total_potential(trap, params), 
-            # (G, params) -> calculate_gradient!(G, trap, params), 
+            (G, params) -> calculate_gradient!(G, trap, params), 
             initial_ion_spacing,
             Optim.Options(iterations = 50000)
         )
     print(res)
-    return Optim.minimizer(res) * 1e6
+    return Optim.minimizer(res)
 end
 
 function get_deviation(ion_positions::Vector{Float64})
@@ -83,11 +83,7 @@ function get_deviation(ion_positions::Vector{Float64})
     return std(ion_spacings)
 end 
 
- 
-trap = TrapVoltage(0, 0.39, 0, 0, 2)
+trap = TrapVoltage(0, 0.398, 0, 0.02, 31)
 spacing = get_ion_spacing_for_voltage(trap)
 scatter(spacing, zeros(trap.num_ion), label="data", minorgrid=true)
 print(spacing)
-
-
-
