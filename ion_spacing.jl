34e3b3ca2f25a2552cfc9a59b51a79dc6fd73859
Vivian Zhang <vivian.zhang@duke.edu>
2023-07-15 12:12:41 -0400

    finishing up ion spacing

--- ion_spacing.jl
+++ ion_spacing.jl
@@ -21,38 +21,42 @@ function get_voltage(trap::TrapVoltage, x::Float64)
     return EURIQA_v0 * (trap.x1 * x′ + trap.x2 * x′^2 / 2.0 + trap.x3 * x′^3 / 6.0 + trap.x4 * x′^4 / 24.0)
 end
 
-function get_coulomb_potential_at_x(ion_positions::Vector{Float64}, x::Float64)
-    potential = 0
-    for ion_pos in ion_positions
-        if ion_pos != x
-            potential = potential + ELECTRON_CHARGE / (4π * PERMITTIVITY * abs(ion_pos - x))
+function get_coulomb_potential_at_x(ion_positions::Vector{Float64}, ion_index::Int64)
+    # Calculate coulomb potential at one ion's position from other ions 
+    potential = 0.0
+    x = ion_positions[ion_index]
+    for i in eachindex(ion_positions)
+        if i != ion_index
+            potential = potential + ELECTRON_CHARGE / (4π * PERMITTIVITY * abs(ion_positions[i] - x))
         end
     end
     return potential
 end
 
 function get_total_potential(trap::TrapVoltage, ion_positions::Vector{Float64})
-    total_potential = 0
+    total_potential = 0.0
     for i in eachindex(ion_positions)
         trap_potential = get_voltage(trap, ion_positions[i])
         coulomb_potential = get_coulomb_potential_at_x(ion_positions, ion_positions[i])
         # divide by two because we counted twice
-        total_potential = total_potential + trap_potential + coulomb_potential / 2
+        total_potential = total_potential + trap_potential + coulomb_potential / 2.0
     end
     return total_potential
 end
 
 function calculate_gradient!(G, trap::TrapVoltage, ion_positions::Vector{Float64})
+    # Calculate total potential's gradient wrt each ion position 
     for i in eachindex(G)
         x = ion_positions[i] / EURIQA_x0
-        trap_gradient = EURIQA_v0 * (trap.x1 + trap.x2 * x + trap.x3 * x^2/2 + trap.x4 * x^3/6)
-        coulomb_gradient = 0
-        for ion in ion_positions
-            if ion != ion_positions[i]
-                coulomb_gradient = coulomb_gradient - (ion - ion_positions[i])
+        trap_gradient = EURIQA_v0 * (trap.x1 + trap.x2 * x + trap.x3 * x^2 / 2.0 + trap.x4 * x^3 / 6.0)
+        coulomb_gradient = 0.0
+        for j in eachindex(ion_positions)
+            if j != i
+                distance = ion_positions[j] - ion_positions[i]
+                coulomb_gradient = coulomb_gradient + sign(distance) / distance^2
             end 
         end 
-        G[i] = trap_gradient + ELECTRON_CHARGE / (4π * PERMITTIVITY) * coulomb_gradient 
+        G[i] = trap_gradient + ELECTRON_CHARGE / (4π * PERMITTIVITY) * coulomb_gradient
     end 
 end 
 
@@ -62,15 +66,28 @@ function get_ion_spacing_for_voltage(trap::TrapVoltage)
     initial_ion_spacing = collect(LinRange(-num_ion / 2, num_ion / 2, num_ion)) * 1e-6
     res = optimize(
             params -> get_total_potential(trap, params), 
-            (G, params) -> calculate_gradient!(G, trap, params), 
-            initial_ion_spacing
+            # (G, params) -> calculate_gradient!(G, trap, params), 
+            initial_ion_spacing,
+            Optim.Options(iterations = 50000)
         )
     print(res)
     return Optim.minimizer(res) * 1e6
 end
+
+function get_deviation(ion_positions::Vector{Float64})
+    num_ion = length(ion_positions)
+    ion_spacings = Array{Float64}(undef, num_ion) 
+    for i in 2:num_ion
+        ion_spacings[i] = ion_positions[i] - ion_positions[i-1]
+    end
+    return std(ion_spacings)
+end 
+
  
-trap = TrapVoltage(0, 0.1, 0, 0, 31)
+trap = TrapVoltage(0, 0.39, 0, 0, 2)
 spacing = get_ion_spacing_for_voltage(trap)
 scatter(spacing, zeros(trap.num_ion), label="data", minorgrid=true)
+print(spacing)
+
 
 
